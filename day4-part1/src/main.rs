cd1a551ab7f0fe387194df168b2cbf434f5c4f8d
Citty <8428080+cittyinthecloud@users.noreply.github.com>
2023-12-04 23:07:54 -0500

    Day 4

---
+++ day4-part1/src/main.rs
@@ -0,0 +1,87 @@
+#![feature(test)]
+extern crate test;
+use std::fs;
+use memchr::memchr;
+struct NumberBitSet([u64; 2]);
+
+impl NumberBitSet {
+    #[inline(always)]
+    fn new() -> Self {
+        Self([0, 0])
+    }
+    
+    #[inline(always)]
+    fn contains(&mut self, i: u8) -> bool {
+        self.0[((i as usize & 64) != 0) as usize] & (1 << (i & 63)) != 0 
+    }
+
+    #[inline(always)]
+    fn set(&mut self, i: u8) {
+        self.0[((i as usize & 64) != 0) as usize] |= 1 << (i & 63);
+    }
+
+    #[inline(always)]
+    fn clear(&mut self) {
+        self.0 = [0, 0];
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input").unwrap();
+
+    println!("{}",do_aoc(&input));
+}
+
+
+
+fn do_aoc(input: &str) -> i32 {
+    let mut sum = 0;
+    let mut bit_set = NumberBitSet::new();
+    for line in input.lines() {
+        let mut num = 0;
+        let mut parsing_our_card = false;
+        let mut res = 0;
+        //println!("{line}");
+
+        let memchr = memchr(b':', line.as_bytes()).unwrap();
+        for b in line[memchr+1..line.len()].bytes() {
+            if !parsing_our_card {
+                if b == b'|' {
+                    //println!("end of winning numbers");
+                    parsing_our_card = true;
+                } else if b == b' ' {
+                    if num != 0 {
+                        //println!("adding {} to set", num);
+                        bit_set.set(num);
+                        num = 0;
+                    }
+                } else if b >= b'0' && b <= b'9' {
+                    //println!("num: {}, d: {}", num, b as char);
+                    num = num * 10 + (b - b'0');
+                }
+            } else if b == b' ' {
+                if bit_set.contains(num) {
+                    res = 1.max(res*2)
+                }
+                num = 0;
+            } else if b >= b'0' && b <= b'9' {
+                num = num * 10 + (b - b'0');
+            }
+        }
+
+        if bit_set.contains(num) {
+            res = 1.max(res*2)
+        }        
+        //println!("{res}");
+        sum+=res;
+        bit_set.clear();
+    }
+    sum
+}
+
+#[bench]
+fn bench_day4(b: &mut test::Bencher) {
+    let file = fs::read_to_string("input").unwrap();
+    
+    b.iter(|| do_aoc(&file));
+}
---
+++ day4-part2/src/main.rs
@@ -0,0 +1,104 @@
+#![feature(test)]
+extern crate test;
+use std::fs;
+struct NumberBitSet([u64; 2]);
+
+impl NumberBitSet {
+    #[inline(always)]
+    fn new() -> Self {
+        Self([0, 0])
+    }
+    
+    #[inline(always)]
+    fn contains(&mut self, i: u8) -> bool {
+        self.0[((i as usize & 64) != 0) as usize] & (1 << (i & 63)) != 0 
+    }
+
+    #[inline(always)]
+    fn set(&mut self, i: u8) {
+        self.0[((i as usize & 64) != 0) as usize] |= 1 << (i & 63);
+    }
+
+    #[inline(always)]
+    fn clear(&mut self) {
+        self.0 = [0, 0];
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input.example").unwrap();
+
+    println!("{}",do_aoc(&input));
+}
+
+fn do_aoc(input: &str) -> u32 {
+    let mut cards: [u32; 256] = [0; 256];
+    cards[0] = 1;
+
+    // Per line
+    let mut bit_set = NumberBitSet::new();
+    let mut line_num: usize = 0;
+    let mut num: u8 = 0;
+    let mut parsing_our_card = false;
+    let mut finds = 0;
+    let mut past_colon = false;
+
+    for b in input.bytes() {
+        if b == b':' {
+            past_colon = true;
+        } else if b == b'\n' {
+            if bit_set.contains(num) {
+                cards[line_num+finds+1] += cards[line_num];
+            }        
+            bit_set.clear();
+            num = 0;
+            finds = 0;
+            parsing_our_card = false;
+            line_num += 1;
+            cards[line_num] += 1;
+            past_colon = false;
+        } else if past_colon {
+            if !parsing_our_card {
+                if b == b'|' {
+                    parsing_our_card = true;
+                } else if b == b' ' {
+                    if num != 0 {
+                        bit_set.set(num);
+                        num = 0;
+                    }
+                } else if b >= b'0' && b <= b'9' {
+                    num = num * 10 + (b - b'0');
+                }
+            } else if b == b' ' {
+                if bit_set.contains(num) {
+                    finds += 1;
+                    cards[line_num+finds] += cards[line_num];
+                }
+                num = 0;
+            } else if b >= b'0' && b <= b'9' {
+                num = num * 10 + (b - b'0');
+            }
+        } 
+    }
+
+    // LLVM please
+
+    const LANES: usize = 8;
+    let chunks = cards.chunks_exact(16);
+    
+    let partial_sums = chunks.fold([0u32; LANES], |mut acc, chunk| {
+        for i in 0..LANES {
+            acc[i] += chunk[i];
+        }
+        acc
+    });
+
+    partial_sums.iter().sum()
+}
+
+#[bench]
+fn bench_day4(b: &mut test::Bencher) {
+    let file = fs::read_to_string("input").unwrap();
+    
+    b.iter(|| do_aoc(&file));
+}
